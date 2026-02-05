8873da96fc451d9f8ab45486e5a9c9063281f335
naveen <naveenveera1201@gmail.com>
2026-02-05 23:38:18 +0530

    1. Find SmallestOrLargest 2. Find Second Largest Smallest

---
+++ FindSecondLargestOrSmallestElement.kt
@@ -0,0 +1,49 @@
+fun main() {
+    val array = arrayOf(11,4,24,56,12,75,67,1,5,87,15,65)
+    secondLargestOrSmallestElementOP(array)
+}
+
+/**
+ * Optimal Way O(N) - Time Complexity
+ * O(N) - Space Complexity
+ */
+fun secondLargestOrSmallestElementOP(value: Array<Int>) {
+    var firstLargest : Int
+    var secondLargest : Int
+
+    var firstSmallest : Int
+    var secondSmallest : Int
+
+    if(value[0] > value[1]) {
+        firstLargest = value[0]
+        secondLargest = value[1]
+        firstSmallest = value[1]
+        secondSmallest = value[0]
+    } else {
+        firstLargest = value[1]
+        secondLargest = value[0]
+        firstSmallest = value[0]
+        secondSmallest = value[1]
+    }
+
+    for(i in 2..<value.size) {
+        if (value[i] > firstLargest) {
+            val temp = firstLargest
+            firstLargest = value[i]
+            secondLargest = temp
+        } else if (value[i] > secondLargest){
+            secondLargest = value[i]
+        }
+
+        if (value[i] < firstSmallest) {
+            val temp = firstSmallest
+            firstSmallest = value[i]
+            secondSmallest = temp
+        } else if (value[i] < secondSmallest){
+            secondSmallest = value[i]
+        }
+    }
+
+    println("Second Largest :: $secondLargest")
+    println("Second Smallest :: $secondSmallest")
+}
---
+++ FindSmallestOrLargest.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val array = arrayOf(11,4,24,56,12,75,1,5,87,15,65)
+    findSmallestOrLargestOP(array)
+}
+
+/**
+ * Optimal Way O(N) - Time Complexity
+ * O(N) - Space Complexity
+ */
+fun findSmallestOrLargestOP(value : Array<Int>) {
+    var smallest = value[0]
+    var largest = value[0]
+
+    for (i in 1..<value.size) {
+        if (value[i] < smallest) {
+            smallest = value[i]
+        }
+
+        if (value[i] > largest) {
+            largest = value[i]
+        }
+    }
+
+    println("Largest -> $largest")
+    println("Smallest -> $smallest")
+
+}
