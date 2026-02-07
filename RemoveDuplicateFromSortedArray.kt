eff68ef7663a4054b47589371ca9e6eacf672f53
Sangili <sangilimurugan492@gmail.com>
2026-02-08 00:00:22 +0530

    1. Find SmallestOrLargest 2. Find Second Largest Smallest

---
+++ RemoveDuplicateFromSortedArray.kt
@@ -0,0 +1,80 @@
+fun main() {
+    println("Brute Force ")
+removeDuplicateFromSortedArrayBF(arrayOf(0,0,1,1,1,2,2,3,3,4))
+
+    println("Using the Extra Data Structure ")
+    removeDuplicateFromSortedArrayDS(arrayOf(0,0,1,1,1,2,2,3,3,4))
+
+    println("Using the Optimal Approch ")
+    removeDuplicateFromSortedArrayOP(arrayOf(0,0,1,1,1,2,2,3,3,4))
+}
+
+/**
+ * Using Another Array and store unique value
+ * Time Complexity O(N)
+ * Space Complexity O(2N) ->
+ */
+fun removeDuplicateFromSortedArrayBF(array: Array<Int>) {
+    val size = array.size
+    val result = arrayOfNulls<Int>(size)
+    var current = array[0]
+    result[0] = array[0]
+    var count = 1
+
+    for (i in 0 until size) {
+        if (array[i] != current) {
+            result[count] = array[i]
+            count++
+        }
+        current = array[i]
+    }
+
+    var resultSize = 0
+    result.forEach {
+        if(it != null) {
+            resultSize++
+        }
+    }
+    println(resultSize)
+
+}
+
+/**
+ * Using Set Data Structure nd store unique value
+ * Time Complexity O(NLogN)
+ * Space Complexity O(2N) ->
+ */
+fun removeDuplicateFromSortedArrayDS(array: Array<Int>) {
+    val size = array.size
+    val resultSet = mutableSetOf<Int> ()
+    for (i in 0 until size) {
+        resultSet.add(array[i])
+    }
+    println(resultSet.size)
+
+}
+
+/**
+ * Using Optimal Approach
+ * Time Complexity O(N)
+ * Space Complexity O(N) ->
+ */
+fun removeDuplicateFromSortedArrayOP(array: Array<Int>) {
+    val size = array.size
+    var count = 0
+    var current = 0
+
+    if (array.isNotEmpty()) {
+        count = 1
+        current = array[0]
+    }
+
+    for (i in 1 until size) {
+        if (current != array[i]) {
+            count++
+        }
+        current = array[i]
+    }
+    println(count)
+
+}
---
+++ out/production/DSA-Learning/README.md
@@ -0,0 +1,6 @@
+Its purely Kotlin Array releated various problems Solutuion based on types
+ 1. Traversal
+ 2. Sliding Window
+ 3. Hashing
+ 4. Cadanes Algoritham
+ 5. Prefix Suffix
