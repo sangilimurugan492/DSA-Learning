ec1d0e5d3fe15acc1b9dcfae18312ff5bffe3a97
Sangili Murugan V <sangilimurugan492@gmail.com>
2026-03-02 22:39:03 +0530

    My Linked List

--- linked_list/MyLinkedList.kt
+++ linked_list/MyLinkedList.kt
@@ -1,10 +1,10 @@
 package linked_list
 
-class MyLinkedList {
-    var head : Node? = null
+class MyLinkedList(node: ListNode) {
+    private var head : ListNode? = node
 
     fun addNodeFirst(data : Int) {
-        val node = Node(data, null)
+        val node = ListNode(data, null)
         if (head == null) {
             head = node
         } else {
@@ -13,7 +13,16 @@ class MyLinkedList {
         }
     }
 
-    fun getMiddleNode(head : Node?) : Int {
+    fun addNode(data : Int) {
+        val node = ListNode(data, null)
+        if (head == null) {
+            head = node
+        } else {
+            head?.next = node
+        }
+    }
+
+    fun getMiddleNode(head : ListNode?) : Int {
         if(head != null) {
             var slow = head
             var fast = head
@@ -21,8 +30,53 @@ class MyLinkedList {
                 slow = slow!!.next!!
                 fast = fast.next!!.next!!
             }
-            return slow!!.data
+            return slow!!.`val`
         }
         return -1
+    }
+
+    /**
+     * Example:
+     * var li = ListNode(5)
+     * var v = li.`val`
+     * Definition for singly-linked list.
+     * class ListNode(var `val`: Int) {
+     *     var next: ListNode? = null
+     * }
+     */
+
+
+}
+
+fun add(l1: ListNode?, l2: ListNode?, carry: Int): ListNode? {
+    if(l1 == null && l2 == null && carry == 0) return null
+
+    val value1 = l1?.`val` ?: 0
+    val value2 = l2?.`val` ?: 0
+
+    val sum = value1 + value2 + carry
+    val newCarry = sum / 10
+    val newNodeValue = sum % 10
+
+    val nextNode = add(l1?.next, l2?.next, newCarry)
+
+    return ListNode(newNodeValue, null).apply {
+        next = nextNode
+    }
+}
+fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+    return add(l1, l2, 0)
+}
+
+fun main() {
+    val firstL = ListNode(9, null)
+    val secodeL = ListNode(1, ListNode(9, ListNode(9, ListNode(9,
+        ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, null))))))
+    ))))
+
+    var result = addTwoNumbers(firstL, secodeL)
+    while (result!= null) {
+        print("${result.`val`}")
+        result = result.next
     }
 }
