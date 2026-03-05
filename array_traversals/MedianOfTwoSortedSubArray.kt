b60c6e487760b1a801741be45bc08f6907d27d6c
Sangili Murugan V <sangilimurugan492@gmail.com>
2026-03-05 17:09:39 +0530

    Median Of Two sorted

--- array_traversals/MedianOfTwoSortedSubArray.kt
+++ array_traversals/MedianOfTwoSortedSubArray.kt
@@ -11,6 +11,10 @@ fun main() {
     println(findMedianSortedArrays(intArrayOf(1,2), intArrayOf(3,4)))
 }
 
+/**
+ * Time Complexity O((m+n) * log(m+n))
+ * Space complexity O(m+n)
+ */
 fun findMedianSortedArraysBF(nums1: IntArray, nums2: IntArray): Double {
     val result = IntArray(nums1.size + nums2.size)
     for(i in nums1.indices) {
@@ -34,6 +38,10 @@ fun findMedianSortedArraysBF(nums1: IntArray, nums2: IntArray): Double {
     return 0.0
 }
 
+/**
+ * Time Complexity O(log(m+n))
+ * Space complexity O(m+n)
+ */
 fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
     val m = nums1.size
     val n = nums2.size
@@ -67,4 +75,45 @@ fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
         val mid2 = result[(result.size/2) - 1]
         return (mid1 + mid2).toDouble()/2
     }
+
+}
+
+fun findMedianSortedArraysOp1(nums1: IntArray, nums2: IntArray): Double {
+
+    if (nums1.size > nums2.size)
+        return findMedianSortedArrays(nums2, nums1)
+
+    val m = nums1.size
+    val n = nums2.size
+
+    var low = 0
+    var high = m
+
+    while (low <= high) {
+
+        val partitionX = (low + high) / 2
+        val partitionY = (m + n + 1) / 2 - partitionX
+
+        val maxLeftX = if (partitionX == 0) Int.MIN_VALUE else nums1[partitionX - 1]
+        val minRightX = if (partitionX == m) Int.MAX_VALUE else nums1[partitionX]
+
+        val maxLeftY = if (partitionY == 0) Int.MIN_VALUE else nums2[partitionY - 1]
+        val minRightY = if (partitionY == n) Int.MAX_VALUE else nums2[partitionY]
+
+        if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+
+            return if ((m + n) % 2 == 0) {
+                (maxOf(maxLeftX, maxLeftY) + minOf(minRightX, minRightY)) / 2.0
+            } else {
+                maxOf(maxLeftX, maxLeftY).toDouble()
+            }
+
+        } else if (maxLeftX > minRightY) {
+            high = partitionX - 1
+        } else {
+            low = partitionX + 1
+        }
+    }
+
+    throw IllegalArgumentException()
 }
