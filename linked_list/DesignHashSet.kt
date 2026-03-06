2ba6ac5ce5e0c35ca47093c688df4bfc5e3e39ab
Sangili Murugan V <sangilimurugan492@gmail.com>
2026-03-06 16:01:55 +0530

    Binary to Integer

---
+++ linked_list/DesignHashSet.kt
@@ -0,0 +1,20 @@
+package array_traversals.linked_list
+
+//import linked_list.ListNode
+
+class MyHashSet() {
+
+    fun add(key: Int) {
+//        val newNode = ListNode(key, null)
+    }
+
+    fun remove(key: Int) {
+
+    }
+
+    fun contains(key: Int): Boolean {
+
+        return false
+    }
+
+}
